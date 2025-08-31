b91641f9cc5b5f98e5b13345fafc47c8ac35319a
Waffen Sultan <waffen.sultan@gmail.com>
2025-08-31 22:03:07 +0800

    Add flash.nvim and disable mini.surround temporarily

--- init.lua
+++ init.lua
@@ -508,7 +508,7 @@ require('lazy').setup({
             -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
             -- - sd'   - [S]urround [D]elete [']quotes
             -- - sr)'  - [S]urround [R]eplace [)] [']
-            require('mini.surround').setup()
+            -- require('mini.surround').setup() -- TEMPORARILY DISABE IN FAVOR OF FLASH
         end,
     },
     { -- Highlight, edit, and navigate code
