0d31ee88daa2d5ac7bed0f8ed4b11f0a808b6fae
Waffen Sultan <waffen.sultan@gmail.com>
2025-09-05 20:34:03 +0800

    Update

--- lua/custom/plugins/alpha-nvim.lua
+++ lua/custom/plugins/alpha-nvim.lua
@@ -53,7 +53,7 @@ return {
             dashboard.button("e", "󰈔  New file", "<cmd>ene<CR>"),
             dashboard.button("h", "  Recent files", telescope_builtin("oldfiles")),
             dashboard.button("p", "󰉋  Projects", telescope_extension("project", "project")),
-            dashboard.button("s", "󰦛  Restore Session", ":AutoSession search<CR>"),
+            dashboard.button("s", "󰦛  Restore session", ":AutoSession search<CR>"),
             dashboard.button("c", "󰒓  Config", "<cmd>edit $MYVIMRC<CR>"),
             dashboard.button("q", "  Quit", "<cmd>qa<CR>"),
         }
