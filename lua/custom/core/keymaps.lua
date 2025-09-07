29dd317672dac468149b61cc16bce1fe05425980
Waffen Sultan <waffen.sultan@gmail.com>
2025-09-07 15:54:08 +0800

    Update lualine to show macros

--- lua/custom/core/keymaps.lua
+++ lua/custom/core/keymaps.lua
@@ -5,8 +5,8 @@ vim.g.maplocalleader = ' '
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Bufferline
-vim.keymap.set('n', ']b', ':BufferLineCycleNext<CR>', { desc = 'Next [B]uffer' })
-vim.keymap.set('n', '[b', ':BufferLineCyclePrev<CR>', { desc = 'Previous [B]uffer' })
+vim.keymap.set('n', ']b', ':bnext<CR>', { desc = 'Next [B]uffer' })
+vim.keymap.set('n', '[b', ':bprev<CR>', { desc = 'Previous [B]uffer' })
 vim.keymap.set('n', '<leader>x', ':Bdelete<CR>', { desc = 'Close [B]uffer' })
 vim.keymap.set('n', '<leader>bb', '<cmd>Telescope buffers<cr>', { desc = 'Find [B]uffers' })
 
--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -16,6 +16,13 @@ return {
                         right_padding = 2
                     }
                 },
+                lualine_x = {
+                    {
+                        require("noice").api.statusline.mode.get,
+                        cond = require("noice").api.statusline.mode.has,
+                        color = { fg = "#ff9e64" },
+                    }
+                },
                 lualine_y = {
                     {
                         "progress",
